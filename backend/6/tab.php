f6b1fec54006569f4d00970c3920888d4fb62a5e
Dill <60818081+Di-Liks@users.noreply.github.com>
2023-05-06 18:21:21 +0300

    Update tab.php

--- backend/6/tab.php
+++ backend/6/tab.php
@@ -64,7 +64,7 @@ body{
               echo '</td>
               <td>'.$user['bio'].'</td>
               <td>
-                <form method="get" action="ind.php">
+                <form method="get" action="index.php">
                   <input name=edit_id value='.$user['id'].' hidden>
                   <input type="submit" value=Выйти>
                 </form>
